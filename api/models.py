e03e54fbad033b34717c812576071ebfeeaacc5d
GabrielRubio <GabrielRubio>
2018-12-06 15:13:22 -0200

    create models to distances and maps

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Maps(models.Model):
+    name = models.CharField(max_length=150, unique=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Maps'
+
+class Distances(models.Model):
+    map_name = models.ForeignKey(Maps, on_delete=models.CASCADE)
+    point_A = models.CharField(max_length=150, unique=True, blank=False)
+    point_A = models.CharField(max_length=150, unique=True, blank=False)
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return self.map_name
+
+    class Meta:
+        db_table = 'Distances'
