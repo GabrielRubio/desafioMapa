807ad3b32546d8e5e064e416f09876ae7366b125
GabrielRubio <GabrielRubio>
2018-12-06 23:38:24 -0200

    Add URL to new api

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('saveMap/', views.saveMap),
+    path('shorterDistance/', views.shorterDistance),
 ]
